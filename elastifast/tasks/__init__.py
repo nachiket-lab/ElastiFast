fddf088ede45879529b98e85cc2593313eb98a16
Nachiket <26703835+nachiket-lab@users.noreply.github.com>
2024-12-11 11:29:08 +0530

    Jiralog-ingestion (#8)
    * Added Jira task
    
    * Migration to Abstract class
    
    * Black formatted code and modified the tasks function
    
    * modified the respons params
    
    * Changed to a class driven approach for ES
    
    * modified
    
    * Modified main file for the retry object
    
    * init

--- elastifast/tasks/__init__.py
+++ elastifast/tasks/__init__.py
@@ -1,16 +1,19 @@
-from pydoc import cli
+import re
 import sys
+from pydoc import cli
 
 import ecs_logging
 import elasticapm
 from celery import Celery, current_task, shared_task
 from celery.signals import after_setup_logger
-from elasticsearch.exceptions import ConnectionError, ConnectionTimeout, TransportError
+from elasticsearch.exceptions import (ConnectionError, ConnectionTimeout,
+                                      TransportError)
 
 from elastifast.config import logger, settings
 from elastifast.models.elasticsearch import ElasticsearchClient
 from elastifast.tasks.atlassian import AtlassianAPIClient
-from elastifast.tasks.ingest_es import index_data
+from elastifast.tasks.ingest_es import ElasticsearchIngestData
+from elastifast.tasks.jira import JiraAuditLogIngestor
 from elastifast.tasks.setup_es import ensure_es_deps
 
 esclient = ElasticsearchClient().client
@@ -44,9 +47,20 @@ def setup_tasks(sender, **kwargs):
     )
 
 
-def common_output(res):
-    return {
-        "message": res.get("message"),
+def common_output(data, object=False):
+    if object:
+        _d = {
+            "class": data.__class__.__name__,
+            "message": data.message
+        }
+    elif type(data) == dict and object is not True:
+        _d = {
+            "message": data.get("message"),
+            **{k: v for k, v in data.items() if k != "message"},
+        }
+    else: 
+        _d = {}
+    d= {
         "transaction": {
             "id": elasticapm.get_transaction_id(),
         },
@@ -54,8 +68,9 @@ def common_output(res):
             "id": elasticapm.get_trace_id(),
             "name": current_task.name,
         },
-        **{k: v for k, v in res.items() if k != "message"},
+        **_d
     }
+    return d
 
 
 @shared_task(
@@ -67,8 +82,8 @@ def common_output(res):
 def ingest_data_to_elasticsearch(self, data: dict, dataset: str, namespace: str):
     index_name = f"logs-{dataset}-{namespace}"
     try:
-        res = index_data(esclient=esclient, data=data, index_name=index_name)
-        return common_output(res)
+        client = ElasticsearchIngestData(esclient=esclient, data=data, index_name=index_name)
+        return common_output(data=client, object=True)
     except (ConnectionError, TimeoutError, ConnectionTimeout, TransportError) as e:
         logger.info(
             f"Error of type {type(e)} occured. Retrying task, attempt number: {self.request.retries}/{self.max_retries}"
@@ -78,22 +93,55 @@ def ingest_data_to_elasticsearch(self, data: dict, dataset: str, namespace: str)
         logger.error(
             f"Error of type {type(e)} occured while ingesting data: {e}. Exiting now."
         )
+        raise
 
 
 @shared_task(retry_backoff=True, max_retries=5)
 def ingest_data_from_atlassian(interval: int, dataset: str, namespace: str):
-    client = AtlassianAPIClient(secret_token=settings.atlassian_secret_token)
+    if settings.atlassian_org_id is None or settings.atlassian_secret_token is None:
+        raise ValueError(
+            "Atlassian credentials not found. Please set ATLASSIAN_ORG_ID and ATLASSIAN_SECRET_TOKEN variables."
+        )
+    client = AtlassianAPIClient(
+        org_id=settings.atlassian_org_id, secret_token=settings.atlassian_secret_token, interval=interval
+    )
     try:
-        data = client.get_events(org_id=settings.atlassian_org_id, time_delta=interval)
-        res = {
-            "data": data,
-            "message": f"Data ingested from Atlassian {len(data)} events",
-        }
+        client.get_events()
+        res = common_output(data=client, object=True)
     except Exception as e:
         logger.error(
             f"Error of type {type(e)} occured while polling data from atlassian: {e}. Exiting now."
         )
     ingest_data_to_elasticsearch.delay(
-        data=res["data"], dataset=dataset, namespace=namespace
+        data=client.data, dataset=dataset, namespace=namespace
     )
-    return common_output(res)
+    return res
+
+
+@shared_task(retry_backoff=True, max_retries=5)
+def ingest_data_from_jira(interval: int, dataset: str, namespace: str):
+    if (
+        settings.jira_url is None
+        or settings.jira_username is None
+        or settings.jira_api_key is None
+    ):
+        raise ValueError(
+            "Jira credentials not found. Please set JIRA_ORG_ID, JIRA_USERNAME and JIRA_API_KEY variables."
+        )
+    client = JiraAuditLogIngestor(
+        interval=interval,
+        url=settings.jira_url,
+        username=settings.jira_username,
+        password=settings.jira_api_key,
+    )
+    try:
+        client.get_events()
+        res = common_output(data=client, object=True)
+    except Exception as e:
+        logger.error(
+            f"Error of type {type(e)} occured while polling data from jira: {e}. Exiting now."
+        )
+    ingest_data_to_elasticsearch.delay(
+        data=client.data, dataset=dataset, namespace=namespace
+    )
+    return res
--- elastifast/tasks/ingest_es.py
+++ elastifast/tasks/ingest_es.py
@@ -1,18 +1,35 @@
+from email import message
 from elasticsearch.helpers import BulkIndexError, bulk
 
 from elastifast.config import logger
 
 
-def index_data(esclient, data: list, index_name: str):
-    for item in data:
-        item["_index"] = index_name
-        item["_op_type"] = "create"
-    try:
-        res = bulk(esclient, data)
-    except BulkIndexError as e:
-        logger.error(f"Indexing error while ingesting data: {e.errors}.")
-        raise
-    except Exception as e:
-        logger.error(f"Error of type {type(e)} occured while ingesting data: {e}.")
-        raise
-    return {"ingested_events": {"success": res[0], "failure": res[1]}}
+class ElasticsearchIngestData:
+    def __init__(self, esclient, data: list, dataset: str, namespace: str):
+        self.esclient = esclient
+        self.data = data
+        self.index_name = f"logs-{dataset}-{namespace}"
+        self.run()
+
+    def _prep_data(self):
+        for item in self.data:
+            item["_index"] = self.index_name
+            item["_op_type"] = "create"
+    
+    def run(self):
+        try:
+            self._prep_data()
+        except Exception as e:
+            self.message = f"Error of type {type(e)} occured while prepping the data: {e}."
+            raise
+        try:
+            res = bulk(self.esclient, self.data)
+            self.message = f"Data ingested by {self.esclient.__class__.__name__}:  success={res[0]} events, failure={res[1]} events"
+        except BulkIndexError as e:
+            self.message = f"Indexing error while ingesting data: {e.errors}."
+            logger.error(message)
+            raise
+        except Exception as e:
+            self.message = f"Error of type {type(e)} occured while ingesting data: {e}."
+            logger.error(message)
+            raise
